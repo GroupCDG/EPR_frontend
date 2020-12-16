56f8860795874dc10e2481ca145e36b4e450f140
cuginoAle <alessio.carnevale@gmail.com>
2020-12-16 20:23:34 +0100

    adding ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import Home from "pages/home";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }

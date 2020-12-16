c50eb667864c1d127fc097211133e4d9fcac8300
cuginoAle <alessio.carnevale@gmail.com>
2020-12-16 22:58:53 +0100

    removing App wrapper

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from "react";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 

66340bff0dce71d8c17d81b5907c1d0af661cc42
cuginoAle <alessio.carnevale@gmail.com>
2021-01-25 23:27:21 +0100

    updating storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import CssBaseline from '../src/theme/Baseline';
 import { ThemeProvider } from 'styled-components';
-import theme, { Typography } from '../src/theme/Default';
+import theme, { Typography } from '../src/theme/default';
 // import { useForm, FormProvider } from "react-hook-form";
 
 addDecorator((storyFn) => {

f63d638299ba55c6e67091f9c82a1171ab6a6d86
cuginoAle <alessio.carnevale@gmail.com>
2021-04-30 15:38:33 +0200

    adding base components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from 'theme/baseline';
-import { Typography } from 'theme/default';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from 'theme/baseline'
+import { Typography } from 'theme/default'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +10,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
